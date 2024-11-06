0289a4338c65348c0282b2991fcbcf234ae6140e
Arnold-Rate <arnoldratemo@gmail.com>
2024-11-06 14:19:54 +0300

    Possible_packages

---
+++ package_uploads.R
@@ -0,0 +1,23 @@
+library(tidyverse) # metapackage of all tidyverse packages
+library(ggplot2)
+library(dplyr)
+library(reshape2) # Melt
+library(plyr)
+
+library(scales) # visualisation
+library(corrplot) # visualisation
+library(GGally) # visualisation
+library(ggthemes) # visualisation
+library(ggalt) # encircle
+library(maps) #maps
+library(treemap)
+library(ggdendro) # Dendogram
+
+# Interactivity
+library(crosstalk)
+library(plotly)
+
+#Date
+library(scales)
+library(zoo)
+library(lubridate)
